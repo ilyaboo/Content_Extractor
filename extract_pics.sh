1cba29e777bb471264e2126382d6047ba0e0e526
ilyaboo <ibouligin@gmail.com>
2023-04-19 16:49:09 -0400

    added the messages and functionaly for reading uyser's input

--- extract_pics.sh
+++ extract_pics.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "hey!"
+echo "Enter the absolute path to the directory from which you want to extract pictures:"
+read
+echo -e "\nPictures will be extracted from the following directory:"
+echo -e $REPLY"\n"
+echo -e "Starting extraction...\n"
