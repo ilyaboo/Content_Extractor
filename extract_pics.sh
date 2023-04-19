230bc81079802b20a0b5b9cb566cae6a9d9aacc5
ilyaboo <ibouligin@gmail.com>
2023-04-19 17:00:45 -0400

    added user input validation

--- extract_pics.sh
+++ extract_pics.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# reading user's input (path)
 echo "Enter the absolute path to the directory from which you want to extract pictures:"
-read
+
+
+while true; do
+    read
+    if test -d $REPLY; then
+        break
+    else
+        echo -e "\n"$REPLY" is not a valid absolute path!"
+        echo -e "Please try again.\n"
+    fi
+done
+
+path=$REPLY
 echo -e "\nPictures will be extracted from the following directory:"
-echo -e $REPLY"\n"
+echo -e $path"\n"
 echo -e "Starting extraction...\n"
+
