1de3bf0b78943d541a4dcc6de809263ac3737aa9
ilyaboo <ibouligin@gmail.com>
2023-04-19 21:08:12 -0400

    added code that searches for pictures and extracts them to the folder

--- extract_pics.sh
+++ extract_pics.sh
@@ -2,11 +2,9 @@
 
 # reading user's input (path)
 echo "Enter the absolute path to the directory from which you want to extract pictures:"
-
-
 while true; do
     read
-    if test -d $REPLY; then
+    if test -d $REPLY; then   # checking if the path is valid
         break
     else
         echo -e "\n"$REPLY" is not a valid absolute path!"
@@ -19,3 +17,13 @@ echo -e "\nPictures will be extracted from the following directory:"
 echo -e $path"\n"
 echo -e "Starting extraction...\n"
 
+mkdir pictures_extracted
+
+formats_pic=(".jpeg" ".jpg" ".png" ".heif" ".bmp")
+
+for format in ${formats_pic[@]}; do   # iterrating over picture formats
+    mkdir pictures_extracted/$format   # creating a directory for a format
+    find . -type f -name "*$format" -exec cp {} pictures_extracted/$format \;
+done
+
+echo "Done!"
