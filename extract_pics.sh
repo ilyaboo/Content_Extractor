a7a120812cee7530442ecfb974eef3117cd93f87
ilyaboo <ibouligin@gmail.com>
2023-04-19 21:41:32 -0400

    added video content extraction

--- extract_pics.sh
+++ extract_pics.sh
@@ -17,6 +17,8 @@ echo -e "\nMedia content will be extracted from the following directory:"
 echo -e $path"\n"
 echo -e "Starting extraction...\n"
 
+
+
 # setting up parameters for image extraction
 mkdir images_extracted   # createing directory for extracted images
 cd images_extracted
@@ -24,10 +26,12 @@ formats_img=("jpeg" "jpg" "png" "heif" "bmp")   # image formats script extracts
 
 for format in ${formats_img[@]}; do   # iterrating over picture formats
     mkdir $format   # creating a directory for a format
-    find $path -type f -name "*.$format" -exec cp {} $format \;   # searching and copying images of the format
+    find $path -type f -iname "*.$format" -exec cp {} $format \;   # searching and copying images of the format
     if [ -z "$(ls $format)" ]; then   # checking if we extracted any images of the format
-        echo "no images of type $format"
+        echo "No images of type: $format"
         rmdir $format   # no images of this format
+    else
+        echo "Ectracted images of type: $format"
     fi
 done
 
@@ -35,6 +39,36 @@ cd ..
 
 if [ -z "$(ls images_extracted)" ]; then   # checking if we extracted any images of any format
     rmdir images_extracted   # no images in total
+    echo "NO IMAGES FOUND"
 fi
+
+echo
+
+
+
+# setting up parameters for video extraction
+mkdir videos_extracted   # createing directory for extracted vidoes
+cd videos_extracted
+formats_vid=("mp4" "mov" "wmv" "avi" "flv" "mkv" "webm")   # vidoe formats script extracts
+
+for format in ${formats_vid[@]}; do   # iterrating over video formats
+    mkdir $format   # creating a directory for a format
+    find $path -type f -iname "*.$format" -exec cp {} $format \;   # searching and copying vidoes of the format
+    if [ -z "$(ls $format)" ]; then   # checking if we extracted any video of the format
+        echo "No vidoes of type: $format"
+        rmdir $format   # no videos of this format
+    else
+        echo "Ectracted vidoes of type: $format"
+    fi
+done
+
+cd ..
+
+if [ -z "$(ls videos_extracted)" ]; then   # checking if we extracted any videos of any format
+    rmdir videos_extracted   # no videos in total
+    echo "NO VIDEOS FOUND"
+fi
+
+echo
 
 echo "Done!"
