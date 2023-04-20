cc22909e9aad2bb603b61eac14547eb379940828
ilyaboo <ibouligin@gmail.com>
2023-04-20 01:05:05 -0400

    added extraction of documents, increased the number of supported formats

--- extract_content.sh
+++ extract_content.sh
@@ -22,7 +22,12 @@ echo -e "\033[32mStarting extraction...\033[0m\n"
 # setting up parameters for image extraction
 mkdir images_extracted   # createing directory for extracted images
 cd images_extracted
-formats_img=("jpeg" "jpg" "png" "heif" "bmp")   # image formats script extracts
+
+# image formats script extracts
+formats_img=("jpg" "jpeg" "png" "svg" "gif" \
+            "bmp" "tiff" "webp" "heif" "ai" \
+            "eps" "psd" "xcf" "ico" "icns" \
+            "pcx" "dng" "orf" "cr2")
 
 for format in ${formats_img[@]}; do   # iterrating over picture formats
     mkdir $format   # creating a directory for a format
@@ -49,7 +54,12 @@ echo
 # setting up parameters for video extraction
 mkdir videos_extracted   # createing directory for extracted vidoes
 cd videos_extracted
-formats_vid=("mp4" "mov" "wmv" "avi" "flv" "mkv" "webm")   # vidoe formats script extracts
+
+# vidoe formats script extracts
+formats_vid=("mp4" "avi" "mov" "wmv" "flv" \
+            "mkv" "mpg" "3gp" "webm" "m4v" \
+            "vob" "mts" "m2ts" "divx" "ogg" \
+            "ogv" "mov" "rm" "rmvb" "asf")   
 
 for format in ${formats_vid[@]}; do   # iterrating over video formats
     mkdir $format   # creating a directory for a format
@@ -67,6 +77,38 @@ cd ..
 if [ -z "$(ls videos_extracted)" ]; then   # checking if we extracted any videos of any format
     rmdir videos_extracted   # no videos in total
     echo -e "\n\033[31mNO VIDEOS FOUND\033[0m"
+fi
+
+echo
+
+
+
+# setting up parameters for document extraction
+mkdir documents_extracted   # createing directory for extracted documents
+cd documents_extracted
+
+# documents formats script extracts
+formats_doc=("pdf" "docx" "pptx" "xlsx" "doc" \
+            "ppt" "xls" "txt" "rtf" "odt" \
+            "ods" "odp" "pages" "numbers" "key" \
+            "md" "tex" "csv" "log" "xml")   
+
+for format in ${formats_doc[@]}; do   # iterrating over documents formats
+    mkdir $format   # creating a directory for a format
+    find $path -type f -iname "*.$format" -exec cp {} $format \;   # searching and copying documents of the format
+    if [ -z "$(ls $format)" ]; then   # checking if we extracted any document of the format
+        echo "No documents of type: $format"
+        rmdir $format   # no documents of this format
+    else
+        echo -e "\033[32mEctracted documents of type: $format\033[0m"
+    fi
+done
+
+cd ..
+
+if [ -z "$(ls documents_extracted)" ]; then   # checking if we extracted any documents of any format
+    rmdir documents_extracted   # no documents in total
+    echo -e "\n\033[31mNO DOCUMENTS FOUND\033[0m"
 fi
 
 echo
